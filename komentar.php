914147015f957ec888dced771d5ef40631a7dd57
Fadhilah Fazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-10-01 10:57:04 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,7 +1,18 @@
-<?php
-//untuk yg komentar kalian bebas mau menggunakan 2 file atau satu file php
+<form action="" method="post">
+ 	<h1>Masukan Komentar</h1>
+ 	Komentar : <textarea name="komentar" rows="5" cols="40"></textarea><br>
+ 	<input type="submit" name="submit" value="Kirim">
+ </form>
 
+ <?php 
+if (isset($_POST['submit'])) {
+	$komentar = $_POST['komentar'];
 
-
-
+	$count = explode(" ", $komentar);
+	if (count($count) < 5) {
+		echo "Jumlah kata tidak sampai 5 karakter";
+	} else {
+		echo "Jumlah kata di komentar : ".count($count)." kata";
+	}
+}
 ?>
